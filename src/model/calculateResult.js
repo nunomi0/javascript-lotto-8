4360733089c8685e0b65a6d4ea1e366b9610af43
nunomi0 <ykys1@naver.com>
2025-11-03 21:46:21 +0900

    refactor(calculateresult): 로또 결과 계산 로직 함수 분리
    - calculateResult() -> initResult(), analyzeLotto(), findPrize()로 분리

--- src/model/calculateResult.js
+++ src/model/calculateResult.js
@@ -1,21 +1,30 @@
 import { PRIZE_TABLE } from "./constants.js";
 
 export default function calculateResult(lottos, winningNumbers, bonusNumber) {
-  const result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+  const result = initResult();
 
   lottos.forEach((lotto) => {
-    const lottoNumbers = lotto.getNumbers();
-    const matchCount = lottoNumbers.filter((num) =>
-      winningNumbers.includes(num)
-    ).length;
-    const hasBonus = lottoNumbers.includes(bonusNumber);
-
-    const prize = PRIZE_TABLE.find(
-      (prize) => prize.match === matchCount && (!!prize.bonus === hasBonus)
-    );
-
+    const { matchCount, hasBonus } = analyzeLotto(lotto, winningNumbers, bonusNumber);
+    const prize = findPrize(matchCount, hasBonus);
     if (prize) result[prize.rank]++;
   });
 
   return result;
+}
+
+function initResult() {
+  return { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+}
+
+function analyzeLotto(lotto, winningNumbers, bonusNumber) {
+  const lottoNumbers = lotto.getNumbers();
+  const matchCount = lottoNumbers.filter((num) => winningNumbers.includes(num)).length;
+  const hasBonus = lottoNumbers.includes(bonusNumber);
+  return { matchCount, hasBonus };
+}
+
+function findPrize(matchCount, hasBonus) {
+  return PRIZE_TABLE.find(
+    (p) => p.match === matchCount && (!!p.bonus === hasBonus)
+  );
 }
