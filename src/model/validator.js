f9cc4a3cf34c54a486aba69f66c19d3ea0fa3839
nunomi0 <ykys1@naver.com>
2025-11-03 20:22:23 +0900

    feat(validator): 보너스 번호 검증
    - 1~45 범위를 벗어나는 경우 예외
    - 당첨 번호와 중복인 경우 예외

--- src/model/validator.js
+++ src/model/validator.js
@@ -33,6 +33,15 @@ const validator = {
       throw new Error("[ERROR] 중복된 당첨 번호가 있습니다.");
     }
   },
+
+  validateBonusNumber(bonus, winningNumbers) {
+    if (bonus < 1 || bonus > 45) {
+      throw new Error("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+    if (winningNumbers.includes(bonus)) {
+      throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+    }
+  },
 }
 
 export default validator;
