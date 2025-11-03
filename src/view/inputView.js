c3070446887990b7163ae3afcf861cb091b13706
nunomi0 <ykys1@naver.com>
2025-11-03 20:20:04 +0900

    feat(inputView): 보너스 번호 입력

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -10,6 +10,11 @@ const inputView = {
     const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
     return input;
   },
+
+  async readBonusNumber() {
+    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    return input;
+  },
 }
 
 export default inputView;
