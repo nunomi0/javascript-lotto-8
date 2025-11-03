44238f9da9338692e6280a29deaa5cffca7f4fe7
nunomi0 <ykys1@naver.com>
2025-11-03 20:00:30 +0900

    feat(validator): 당첨 번호 검증
    - 번호가 6개가 아닌 경우 예외
    - 중복된 번호가 있는 경우 예외
    - 1~45 범위를 벗어나는 경우 예외

--- src/model/validator.js
+++ src/model/validator.js
@@ -13,6 +13,26 @@ const validator = {
       throw new Error("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
     }
   },
+
+  validateWinningNumbers(winningNumbers) {
+    if (winningNumbers.length !== 6) {
+      throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
+    }
+
+    const hasDuplicate = new Set(winningNumbers).size !== winningNumbers.length;
+    if (hasDuplicate) {
+      throw new Error("[ERROR] 중복된 당첨 번호가 있습니다.");
+    }
+
+    winningNumbers.forEach((num) => {
+      if (!Number.isInteger(num)) {
+        throw new Error("[ERROR] 당첨 번호는 숫자여야 합니다.");
+      }
+      if (num < 1 || num > 45) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    });
+  },
 }
 
 export default validator;
