62f501c617ebfef10e3d01a7df10c39aca49e45c
nunomi0 <ykys1@naver.com>
2026-01-08 08:21:58 +0900

    chore: 입력에 개행 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,14 +2,14 @@ import { Console } from "@woowacourse/mission-utils";
 
 export default class InputView {
     static async readPurchaseAmount() {
-      return Console.readLineAsync("구입금액을 입력해 주세요.");
+      return Console.readLineAsync("구입금액을 입력해 주세요.\n");
     }
 
     static async readWinningNumbers() {
-      return Console.readLineAsync("당첨 번호를 입력해 주세요.")
+      return Console.readLineAsync("당첨 번호를 입력해 주세요.\n")
     }
 
     static async readBonusNumber() {
-      return Console.readLineAsync("보너스 번호를 입력해 주세요.")
+      return Console.readLineAsync("보너스 번호를 입력해 주세요.\n")
     }
 }
