990cdf6de4e13d6f566f40d251c370b53cdb6746
nunomi0 <ykys1@naver.com>
2026-01-09 08:02:32 +0900

    feat(app): 사용자가 잘못된 값을 입력할 경우 에러 문구 출력 및 해당 지점부터 다시 입력

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { validatePurchaseAmount, validateWinningNumbers, validateBonusNumber } f
 import LottoMachine from "./LottoMachine.js";
 import OutputView from "./view/OutputView.js";
 import ResultCalculator from "./ResultCalculator.js";
+import { Console } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
 
-    const purchaseAmount = await InputView.readPurchaseAmount();
-    validatePurchaseAmount(purchaseAmount);
-    
+    const purchaseAmount = await this.getPurchaseAmount();    
     const lottoCount = purchaseAmount/1000;
 
-    const rawWinningNumbers = await InputView.readWinningNumbers();
-    const winningNumbers = validateWinningNumbers(rawWinningNumbers);
-
-    const bonusNumber = await InputView.readBonusNumber();
-    validateBonusNumber(bonusNumber, winningNumbers);
+    const winningNumbers = await this.getWinningNumbers();    
+    const bonusNumber = await this.getBonusNumber(winningNumbers);    
 
     const lottoMachine = new LottoMachine(lottoCount);
     const lottos = lottoMachine.getLottos();
@@ -31,6 +27,39 @@ class App {
     OutputView.printResultHeader();
     OutputView.printRanks(ranks);
     OutputView.printProfitRate(profitRate);
+  }
+
+  async getPurchaseAmount(){
+    try {
+      const purchaseAmount = await InputView.readPurchaseAmount();
+      validatePurchaseAmount(purchaseAmount);
+      return purchaseAmount;
+    } catch (error) {
+      Console.print(error.message);
+      return this.getPurchaseAmount();
+    }
+  }
+
+  async getWinningNumbers(){
+    try {
+      const rawWinningNumbers = await InputView.readWinningNumbers();
+      const winningNumbers = validateWinningNumbers(rawWinningNumbers);  
+      return winningNumbers;
+    } catch (error) {
+      Console.print(error.message);
+      return this.getWinningNumbers();
+    }
+  }
+
+  async getBonusNumber(winningNumbers){
+    try {
+      const bonusNumber = await InputView.readBonusNumber();
+      validateBonusNumber(bonusNumber, winningNumbers);
+      return bonusNumber;
+    } catch (error) {
+      Console.print(error.message);
+      return this.getBonusNumber();
+    }
   }
 }
 
